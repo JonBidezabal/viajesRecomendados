ec6e4a1afe49605e77c3202be3a52dfe870eac71
Karol Bracho <brachokarol.sud@gmail.com>
2023-06-26 18:51:01 +0200

    meti en el homepage la lista de todos los lugares

--- viajesrecomendados/src/pages/HomePage.js
+++ viajesrecomendados/src/pages/HomePage.js
@@ -1,4 +1,19 @@
+import PostsList from "../components/PostsList"
+import { useAllPlaces } from "../hooks"
+
 const HomePage = () => {
+  const info = useAllPlaces()
+  if (!info) return <div>Cargando...</div>
+  if (info.status !== "ok") return <div>No se encontró ninguna experiencia</div>
+  return (
+    <main>
+      <section className="list-all-places">
+        <h2>Reseñas más recientes</h2>
+        <h3>Descubre los siguientes lugares</h3>
+        {info && <PostsList posts={info} />}
+      </section>
+    </main>
+  )
   return (
     <p>HomePage</p>
   )
