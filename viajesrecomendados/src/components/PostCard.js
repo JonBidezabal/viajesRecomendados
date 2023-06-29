97a3fabfd50a5664db62e621f864d60f21453d1e
Karol Bracho <brachokarol.sud@gmail.com>
2023-06-29 19:25:02 +0200

    poniendo el link en toda la tarjeta

--- viajesrecomendados/src/components/PostCard.js
+++ viajesrecomendados/src/components/PostCard.js
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 const PostCard = ({ post }) => {
   return (
     <>
-      {post.photo && <img style={{ width: "500px" }} src={`${process.env.REACT_APP_BACKEND}/${post.photo}`} alt={`Foto de ${post.title}`} />}
       <Link to={`/places/${post.place_id}`}>
+        {post.photo && <img style={{ width: "500px" }} src={`${process.env.REACT_APP_BACKEND}/${post.photo}`} alt={`Foto de ${post.title}`} />}
         <h3 title="Ver este lugar">{post.title}</h3>
+        <p>
+          {post.votes_average
+            ? `${"⭐".repeat(post.votes_average)}${"☆".repeat(
+              5 - post.votes_average
+            )}`
+            : "No hay votos aún"}
+        </p>
       </Link>
-      <p>
-        {post.votes_average
-          ? `${"⭐".repeat(post.votes_average)}${"☆".repeat(
-            5 - post.votes_average
-          )}`
-          : "No hay votos aún"}
-      </p>
     </>
   );
 };
